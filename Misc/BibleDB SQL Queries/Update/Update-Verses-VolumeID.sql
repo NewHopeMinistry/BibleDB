bc97776886004e89aa5e794172b4dbfffbeb95f1
NewHopeMinistry <gbutler27370@live.com>
2024-06-10 17:49:16 -0400

    Add AKJ

--- Misc/BibleDB SQL Queries/Update/Update-Verses-VolumeID.sql
+++ Misc/BibleDB SQL Queries/Update/Update-Verses-VolumeID.sql
@@ -1,2 +1,2 @@
-Update BBEVerses Set VolumeID = 0 Where BookID < 40;
-Update BBEVerses Set VolumeID = 1 Where BookID > 39;
+Update AKJVerses Set VolumeID = 0 Where BookID < 40;
+Update AKJVerses Set VolumeID = 1 Where BookID > 39;
