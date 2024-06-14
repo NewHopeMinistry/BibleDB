5a094cf332fa6457c6c994944c4d09b26e575f8e
NewHopeMinistry <gbutler27370@live.com>
2024-06-14 12:36:42 -0400

    Update Books

---
+++ VerseData/New-American-Bible-(NAB)/NABBooks/NABBooks.js
@@ -0,0 +1,398 @@
+var NABBooks = [
+    {
+        "Abreviation": "GEN",
+        "Book": "Genesis",
+        "BookID": 1,
+        "Chapters": 50
+    },
+    {
+        "Abreviation": "EXO",
+        "Book": "Exodus",
+        "BookID": 2,
+        "Chapters": 40
+    },
+    {
+        "Abreviation": "LEV",
+        "Book": "Leviticus",
+        "BookID": 3,
+        "Chapters": 27
+    },
+    {
+        "Abreviation": "NUM",
+        "Book": "Numbers",
+        "BookID": 4,
+        "Chapters": 36
+    },
+    {
+        "Abreviation": "DEU",
+        "Book": "Deuteronomy",
+        "BookID": 5,
+        "Chapters": 34
+    },
+    {
+        "Abreviation": "JOS",
+        "Book": "Joshua",
+        "BookID": 6,
+        "Chapters": 24
+    },
+    {
+        "Abreviation": "JDG",
+        "Book": "Judges",
+        "BookID": 7,
+        "Chapters": 21
+    },
+    {
+        "Abreviation": "RUT",
+        "Book": "Ruth",
+        "BookID": 8,
+        "Chapters": 4
+    },
+    {
+        "Abreviation": "1SA",
+        "Book": "1 Samuel",
+        "BookID": 9,
+        "Chapters": 31
+    },
+    {
+        "Abreviation": "2SA",
+        "Book": "2 Samuel",
+        "BookID": 10,
+        "Chapters": 24
+    },
+    {
+        "Abreviation": "1KI",
+        "Book": "1 Kings",
+        "BookID": 11,
+        "Chapters": 22
+    },
+    {
+        "Abreviation": "2KI",
+        "Book": "2 Kings",
+        "BookID": 12,
+        "Chapters": 25
+    },
+    {
+        "Abreviation": "1CH",
+        "Book": "1 Chronicles",
+        "BookID": 13,
+        "Chapters": 29
+    },
+    {
+        "Abreviation": "2CH",
+        "Book": "2 Chronicles",
+        "BookID": 14,
+        "Chapters": 36
+    },
+    {
+        "Abreviation": "EZR",
+        "Book": "Ezra",
+        "BookID": 15,
+        "Chapters": 10
+    },
+    {
+        "Abreviation": "NEH",
+        "Book": "Nehemiah",
+        "BookID": 16,
+        "Chapters": 13
+    },
+    {
+        "Abreviation": "EST",
+        "Book": "Esther",
+        "BookID": 17,
+        "Chapters": 10
+    },
+    {
+        "Abreviation": "JOB",
+        "Book": "Job",
+        "BookID": 18,
+        "Chapters": 42
+    },
+    {
+        "Abreviation": "PSA",
+        "Book": "Psalms",
+        "BookID": 19,
+        "Chapters": 150
+    },
+    {
+        "Abreviation": "PRO",
+        "Book": "Proverbs",
+        "BookID": 20,
+        "Chapters": 31
+    },
+    {
+        "Abreviation": "ECC",
+        "Book": "Ecclesiastes",
+        "BookID": 21,
+        "Chapters": 12
+    },
+    {
+        "Abreviation": "SNG",
+        "Book": "Song of Solomon",
+        "BookID": 22,
+        "Chapters": 8
+    },
+    {
+        "Abreviation": "ISA",
+        "Book": "Isaiah",
+        "BookID": 23,
+        "Chapters": 66
+    },
+    {
+        "Abreviation": "JER",
+        "Book": "Jeremiah",
+        "BookID": 24,
+        "Chapters": 52
+    },
+    {
+        "Abreviation": "LAM",
+        "Book": "Lamentations",
+        "BookID": 25,
+        "Chapters": 5
+    },
+    {
+        "Abreviation": "EZK",
+        "Book": "Ezekiel",
+        "BookID": 26,
+        "Chapters": 48
+    },
+    {
+        "Abreviation": "DAN",
+        "Book": "Daniel",
+        "BookID": 27,
+        "Chapters": 12
+    },
+    {
+        "Abreviation": "HOS",
+        "Book": "Hosea",
+        "BookID": 28,
+        "Chapters": 14
+    },
+    {
+        "Abreviation": "JOL",
+        "Book": "Joel",
+        "BookID": 29,
+        "Chapters": 3
+    },
+    {
+        "Abreviation": "AMO",
+        "Book": "Amos",
+        "BookID": 30,
+        "Chapters": 9
+    },
+    {
+        "Abreviation": "OBA",
+        "Book": "Obadiah",
+        "BookID": 31,
+        "Chapters": 1
+    },
+    {
+        "Abreviation": "JON",
+        "Book": "Jonah",
+        "BookID": 32,
+        "Chapters": 4
+    },
+    {
+        "Abreviation": "MIC",
+        "Book": "Micah",
+        "BookID": 33,
+        "Chapters": 7
+    },
+    {
+        "Abreviation": "NAM",
+        "Book": "Nahum",
+        "BookID": 34,
+        "Chapters": 3
+    },
+    {
+        "Abreviation": "HAB",
+        "Book": "Habakkuk",
+        "BookID": 35,
+        "Chapters": 3
+    },
+    {
+        "Abreviation": "ZEP",
+        "Book": "Zephaniah",
+        "BookID": 36,
+        "Chapters": 3
+    },
+    {
+        "Abreviation": "HAG",
+        "Book": "Haggai",
+        "BookID": 37,
+        "Chapters": 2
+    },
+    {
+        "Abreviation": "ZEC",
+        "Book": "Zechariah",
+        "BookID": 38,
+        "Chapters": 14
+    },
+    {
+        "Abreviation": "MAL",
+        "Book": "Malachi",
+        "BookID": 39,
+        "Chapters": 4
+    },
+    {
+        "Abreviation": "MAT",
+        "Book": "Matthew",
+        "BookID": 40,
+        "Chapters": 28
+    },
+    {
+        "Abreviation": "MRK",
+        "Book": "Mark",
+        "BookID": 41,
+        "Chapters": 16
+    },
+    {
+        "Abreviation": "LUK",
+        "Book": "Luke",
+        "BookID": 42,
+        "Chapters": 24
+    },
+    {
+        "Abreviation": "JHN",
+        "Book": "John",
+        "BookID": 43,
+        "Chapters": 21
+    },
+    {
+        "Abreviation": "ACT",
+        "Book": "Acts",
+        "BookID": 44,
+        "Chapters": 28
+    },
+    {
+        "Abreviation": "ROM",
+        "Book": "Romans",
+        "BookID": 45,
+        "Chapters": 16
+    },
+    {
+        "Abreviation": "1CO",
+        "Book": "1 Corinthians",
+        "BookID": 46,
+        "Chapters": 16
+    },
+    {
+        "Abreviation": "2CO",
+        "Book": "2 Corinthians",
+        "BookID": 47,
+        "Chapters": 13
+    },
+    {
+        "Abreviation": "GAL",
+        "Book": "Galatians",
+        "BookID": 48,
+        "Chapters": 6
+    },
+    {
+        "Abreviation": "EPH",
+        "Book": "Ephesians",
+        "BookID": 49,
+        "Chapters": 6
+    },
+    {
+        "Abreviation": "PHP",
+        "Book": "Philippians",
+        "BookID": 50,
+        "Chapters": 4
+    },
+    {
+        "Abreviation": "COL",
+        "Book": "Colossians",
+        "BookID": 51,
+        "Chapters": 4
+    },
+    {
+        "Abreviation": "1TH",
+        "Book": "1 Thessalonians",
+        "BookID": 52,
+        "Chapters": 5
+    },
+    {
+        "Abreviation": "2TH",
+        "Book": "2 Thessalonians",
+        "BookID": 53,
+        "Chapters": 3
+    },
+    {
+        "Abreviation": "1TI",
+        "Book": "1 Timothy",
+        "BookID": 54,
+        "Chapters": 6
+    },
+    {
+        "Abreviation": "2TI",
+        "Book": "2 Timothy",
+        "BookID": 55,
+        "Chapters": 4
+    },
+    {
+        "Abreviation": "TIT",
+        "Book": "Titus",
+        "BookID": 56,
+        "Chapters": 3
+    },
+    {
+        "Abreviation": "PHM",
+        "Book": "Philemon",
+        "BookID": 57,
+        "Chapters": 1
+    },
+    {
+        "Abreviation": "HEB",
+        "Book": "Hebrews",
+        "BookID": 58,
+        "Chapters": 13
+    },
+    {
+        "Abreviation": "JAS",
+        "Book": "James",
+        "BookID": 59,
+        "Chapters": 5
+    },
+    {
+        "Abreviation": "1PE",
+        "Book": "1 Peter",
+        "BookID": 60,
+        "Chapters": 5
+    },
+    {
+        "Abreviation": "2PE",
+        "Book": "2 Peter",
+        "BookID": 61,
+        "Chapters": 3
+    },
+    {
+        "Abreviation": "1JN",
+        "Book": "1 John",
+        "BookID": 62,
+        "Chapters": 5
+    },
+    {
+        "Abreviation": "2JN",
+        "Book": "2 John",
+        "BookID": 63,
+        "Chapters": 1
+    },
+    {
+        "Abreviation": "3JN",
+        "Book": "3 John",
+        "BookID": 64,
+        "Chapters": 1
+    },
+    {
+        "Abreviation": "JUD",
+        "Book": "Jude",
+        "BookID": 65,
+        "Chapters": 1
+    },
+    {
+        "Abreviation": "REV",
+        "Book": "Revelation",
+        "BookID": 66,
+        "Chapters": 22
+    }
+];
