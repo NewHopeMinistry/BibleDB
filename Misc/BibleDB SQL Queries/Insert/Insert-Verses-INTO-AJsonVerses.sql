a823fe3c40083ee2d2ed8636e63ec1cae172e792
NewHopeMinistry <gbutler27370@live.com>
2024-09-17 14:52:11 -0400

    Update TWF

--- Misc/BibleDB SQL Queries/Insert/Insert-Verses-INTO-AJsonVerses.sql
+++ Misc/BibleDB SQL Queries/Insert/Insert-Verses-INTO-AJsonVerses.sql
@@ -1,3 +1,3 @@
 INSERT INTO AJsonVerses (vid, vn, bid, cn, vt, jq, pn)
 SELECT VerseID, VerseNumber, BookID, ChapterNumber, VerseText, JesusQuote,
-ParagraphNumber FROM YLTVerses
+ParagraphNumber FROM TWFVerses
