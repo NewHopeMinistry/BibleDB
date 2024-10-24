365041b93471231a5cfdbf44766672de23f35351
NewHopeMinistry <gbutler27370@live.com>
2024-10-24 14:36:00 -0400

    Update TWF

--- Misc/BibleDB SQL Queries/Select/Select-FROM-Table-WHERE-LIKE-value.sql
+++ Misc/BibleDB SQL Queries/Select/Select-FROM-Table-WHERE-LIKE-value.sql
@@ -1,1 +1,1 @@
-SELECT * FROM TWFVerses WHERE VerseText LIKE "%avouched%"; 
+SELECT * FROM TWFVerses WHERE VerseText LIKE "% Meribah %"; 
--- Misc/BibleDB SQL Queries/Update/UPDATE-string-within-VerseText.sql
+++ Misc/BibleDB SQL Queries/Update/UPDATE-string-within-VerseText.sql
@@ -1,3 +1,3 @@
 UPDATE TWFVerses
 SET VerseText = REPLACE(VerseText,
-"israel's Children", "Israelis");
+"zebulun", "Zebulun");
