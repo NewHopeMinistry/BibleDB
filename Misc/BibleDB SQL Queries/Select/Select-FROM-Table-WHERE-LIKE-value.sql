1f17ff1c4ee321ec495468930098de9110475639
NewHopeMinistry <gbutler27370@live.com>
2024-12-26 15:55:52 -0500

    Update TWF

--- Misc/BibleDB SQL Queries/Select/Select-FROM-Table-WHERE-LIKE-value.sql
+++ Misc/BibleDB SQL Queries/Select/Select-FROM-Table-WHERE-LIKE-value.sql
@@ -1,1 +1,1 @@
-SELECT * FROM TWFVerses WHERE VerseText LIKE "% %"; 
+SELECT * FROM TWFVerses WHERE VerseText LIKE "%from Bethlehem%"; 
--- Misc/BibleDB SQL Queries/Update/UPDATE-string-within-VerseText.sql
+++ Misc/BibleDB SQL Queries/Update/UPDATE-string-within-VerseText.sql
@@ -1,3 +1,3 @@
 UPDATE TWFVerses
 SET VerseText = REPLACE(VerseText,
-"armor bearer", "armor-bearer");
+" the Bethlehemite", " of Bethlehem");
