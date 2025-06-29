ea4e3b56ec95c84a20800760a8346e75ca3d1c6e
NewHopeMinistry <gbutler27370@live.com>
2025-06-29 18:21:28 -0400

    Update TWF

--- Misc/BibleDB SQL Queries/Insert/Insert-AAVersions-INTO-AAAVersions.sql
+++ Misc/BibleDB SQL Queries/Insert/Insert-AAVersions-INTO-AAAVersions.sql
@@ -1,7 +1,6 @@
-INSERT INTO AAAVersions (Title,CopyrightID,Copyright,CountryID,Country,
-LanguageID,Language,Abreviation,Authors,History,Links,VerseCount,
-Strongs,Searchable,RedLetter,Status)
-SELECT Title,CopyrightID,Copyright,CountryID,Country,
-LanguageID,Language,Abreviation,Authors,History,Links,VerseCount,
-Strongs,Searchable,RedLetter,Status
+INSERT INTO AAAAVersions (Title,CopyrightID,Copyright,LanguageID,Language,
+Abreviation,Authors,History,Links,VerseCount,Strongs,Searchable,
+RedLetter,Status)
+SELECT Title,CopyrightID,Copyright,LanguageID,Language,Abreviation,
+Authors,History,Links,VerseCount,Strongs,Searchable,RedLetter,Status
 FROM AAVersions ORDER BY Title ASC;
