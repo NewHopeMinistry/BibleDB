f33a2e44deac77711f73fd0c171ff987209b7440
NewHopeMinistry <gbutler27370@live.com>
2025-06-30 15:56:43 -0400

    Fix ERV & AKJ

---
+++ Misc/BibleDB SQL Queries/Update/Update Deuteronomy & Psalms.sql
@@ -0,0 +1,4 @@
+Update AKJVerses Set BookID = 5 Where VerseID > 4893 AND VerseID < 5853;
+Update AKJVerses Set Book = 'Deuteronomy' Where BookID = 5;
+Update AKJVerses Set BookID = 19 Where BookID = 0;
+Update AKJVerses Set Book = 'Psalms' Where BookID = 19;
